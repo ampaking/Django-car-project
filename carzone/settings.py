a67ad07f485f8a40d521d635ff3287ab8809bae7
ampaking <78628177+ampaking@users.noreply.github.com>
2021-06-28 17:29:59 +0900

    in this commint change the static file to base

--- carzone/settings.py
+++ carzone/settings.py
@@ -9,7 +9,7 @@ https://docs.djangoproject.com/en/3.2/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/3.2/ref/settings/
 """
-
+import os
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -20,7 +20,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-$ptj7*_jrr-*dputtd2e^d5#&0pl2#5^p4ydzmvvjf^*j1)@sb'
+SECRET_KEY = 'django-insecure-+)0ewm1339$ykwo^x#rw6e@!v7cil^4!&pfm%)#64#7vhy8kng'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
@@ -31,6 +31,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+
     'pages.apps.PagesConfig',
     'django.contrib.admin',
     'django.contrib.auth',
@@ -55,9 +56,7 @@ ROOT_URLCONF = 'carzone.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [
-            'templates'
-        ],
+        'DIRS': ['templates'],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -79,7 +78,7 @@ WSGI_APPLICATION = 'carzone.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'NAME': os.path.join(BASE_DIR,'db.sqlite3'),
     }
 }
 
@@ -121,6 +120,10 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
 STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR,'static')
+STATICFILES_DIRS =[
+    os.path.join(BASE_DIR,'carzone/static')
+]
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
--- pages/views.py
+++ pages/views.py
@@ -1,6 +1,19 @@
+from django.http import HttpResponse
 from django.shortcuts import render
 
 # Create your views here.
 
-def index(request):
-    return render(request, 'pages/index.html')
+def home(request):
+    return render(request, 'pages/home.html')
+
+
+def about(request):
+    return render(request, 'pages/about.html')
+
+
+def services(request):
+    return render(request, 'pages/services.html')
+
+
+def contact(request):
+    return render(request, 'pages/contact.html')
