94a50e3c7bbcbb7d7ee2e64c2d1606165fdfaa24
ampaking <78628177+ampaking@users.noreply.github.com>
2021-07-04 15:21:19 +0900

    complete search fields

--- cars/urls.py
+++ cars/urls.py
@@ -6,7 +6,8 @@ from . import views
 
 urlpatterns = [
     path('', views.cars, name='cars'),
-
+    path('<int:id>', views.car_details, name ='car_details',),
+    path('search', views.search, name='search')
 ] 
 
 # + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
