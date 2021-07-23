e606fdb0e6e4e531ccea7b84e0790a48338f1fd7
ampaking <78628177+ampaking@users.noreply.github.com>
2021-07-24 03:12:13 +0900

    login and Register Functions Update

--- account/views.py
+++ account/views.py
@@ -1,15 +1,67 @@
+from django.contrib import messages,auth
 from django.shortcuts import render, redirect
+from django.contrib.auth.models import User
+
 
 # Create your views here.
 def login(request):
+    if request.method== 'POST':
+        username = request.POST['username']
+        password = request.POST['password']
+
+        user= auth.authenticate(username=username,password=password)
+
+        if user is not None:
+            auth.login(request, user)
+            messages.success(request, 'You are Login')
+            return redirect('dashboard')
+        else:
+            messages.error(request,'User name or password not match')
+            return redirect('login')    
+
     return render(request, 'account/login.html')
 def register(request):
     if request.method == 'POST':
-        print('User click register button')
-        return redirect('regester')
+        # print('User click register button')
+        # messages.error(request, 'Some Error happens Check again')
+        # return redirect('register')
+        firstname = request.POST['firstname']
+        lastname = request.POST['lastname']
+        username = request.POST['username']
+        email = request.POST['email']
+        password = request.POST['password']
+        confirm_password = request.POST['confirm_password']
+
+        if password == confirm_password:
+            if User.objects.filter(username=username).exists():
+                messages.error(request, 'User name already exists, please try something unique')
+                return redirect('register')
+            else:
+                if User.objects.filter(email=email).exists():
+                    messages.error(request, 'Email already exists')
+                    return redirect('register')
+                else:
+                    user = User.objects.create_user(username=username, email=email, password=password,first_name=firstname,last_name=lastname)
+
+                        #Django use SHA 252 Hashing tecnic
+                    auth.login(request, user)
+                    messages.success(request,'Your account is created. You are login')
+                    return redirect('dashboard')
+                    user.save()
+                
+                    messages.success(request, 'You are register successfully')
+                    return redirect('login')     
+
+
+        else:
+            messages.error(request,'Password does not match')    
     else:    
         return render(request, 'account/register.html')
 def logout(request):
+    if request.method == 'POST':
+        auth.logout(request)
+        messages.success(request, 'You are successfully logged out')
+        return redirect('home')
     return redirect('home')
 def dashboard(request):
     return render(request, 'account/dashboard.html')
--- carzone/settings.py
+++ carzone/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/3.2/ref/settings/
 """
 import os
 from pathlib import Path
+from django.contrib.messages import constants as messages
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -27,6 +28,9 @@ DEBUG = True
 
 ALLOWED_HOSTS = []
 
+# LOGIN_REDIRECT_URL = 'dashboard'
+
+
 
 # Application definition
 
@@ -42,6 +46,14 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'ckeditor',
     'django.contrib.humanize',
+    'django.contrib.sites',
+    'allauth',
+    'allauth.account',
+    'allauth.socialaccount',
+
+    #Providers
+    'allauth.socialaccount.providers.facebook',
+    'allauth.socialaccount.providers.google',
 ]
 
 MIDDLEWARE = [
@@ -136,10 +148,17 @@ STATICFILES_DIRS =[
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
+#messeges
 
+MESSAGE_TAGS = {
+    messages.ERROR: 'danger',
+}
 
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+
+SITE_ID = 1
